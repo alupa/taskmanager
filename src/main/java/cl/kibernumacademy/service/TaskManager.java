66d6b23528ac7da7188d5c8026d3127da97fb0c4
Alvaro Lupa <alvaro.lupa@gmail.com>
2025-07-09 23:10:13 -0400

    Escenario Refactor con verificable

--- src/main/java/cl/kibernumacademy/service/TaskManager.java
+++ src/main/java/cl/kibernumacademy/service/TaskManager.java
@@ -13,22 +13,58 @@ public class TaskManager {
         return task;
     }
 
+    // GREEN
+    // public boolean updateTask(int id, String newTitle, String newDescription) {
+    //     for (Task task : tasks) {
+    //         if (task.getId() == id) {
+    //             task.setTitle(newTitle);
+    //             task.setDescription(newDescription);
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // REFACTOR
     public boolean updateTask(int id, String newTitle, String newDescription) {
-        for (Task task : tasks) {
-            if (task.getId() == id) {
-                task.setTitle(newTitle);
-                task.setDescription(newDescription);
+        return findTaskById(id)
+            .map(i -> {
+                i.setTitle(newTitle);
+                i.setDescription(newDescription);
                 return true;
-            }
-        }
-        return false;
+            })
+            .orElse(false);
     }
 
+    private Optional<Task> findTaskById(int id) {
+        return tasks.stream()
+            .filter(i -> i.getId() == id)
+            .findFirst();
+    }
+
+    // GREEN
+    // public boolean deleteTask(int id){
+    //     for(Task task : tasks){
+    //         if(task.getId() == id){
+    //             tasks.remove(task);
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // REFACTOR
     public boolean deleteTask(int id) {
         return tasks.removeIf(task -> task.getId() == id);
     }
 
+    // GREEN
+    // public List<Task> getAll() {
+    //     return new ArrayList<>(tasks);
+    // }
+
+    // REFACTOR
     public List<Task> getAll() {
-        return new ArrayList<>(tasks);
+        return Collections.unmodifiableList(tasks);
     }
 } 
